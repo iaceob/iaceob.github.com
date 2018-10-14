10054ccf89aed4b81c167380a76c600a81df4ded
iaceob <iaceob@gmail.com>
2018-10-14 13:53:03 +0800

    eome

--- eome/assets/js/eome.js
+++ eome/assets/js/eome.js
@@ -4,23 +4,51 @@
   var $ = window.document;
   var console = window.console;
   var ix = 1;
-
-  var demos = $.getElementsByClassName('enoa-demo');
-
-  function showDemo(ix) {
-    for (var i = demos.length; i-- > 0;) {
-      var demo = demos[i];
-      if (i === ix) {
-        demo.className = demo.className + ' show';
-        continue;
-      }
-      demo.className = demo.className.replace('show', '');
+  var version = '1.6.2';
+  var wikis = [
+    {
+      url: 'https://doc.enoa.io/' + version + '/zh-tw',
+      lang: '中文 (正體)'
+    },
+    {
+      url: 'https://doc.enoa.io/' + version + '/zh-cn',
+      lang: '中文 (简体)'
     }
+  ];
+
+  var docslangs = [
+    {
+      url: 'https://enoa.io',
+      lang: 'English'
+    },
+    {
+      url: 'https://enoa.io/zh-tw',
+      lang: '中文 (正體)'
+    },
+    {
+      url: 'https://enoa.io/zh-cn',
+      lang: '中文 (简体)'
+    }
+  ];
+
+  function fillVersion() {
+    var v = $.getElementById('version');
+    v.innerText = version;
   }
 
-  window.setInterval(function () {
-    showDemo(ix);
-    ix = ix + 1 > 2 ? 0 : ix + 1;
-  }, 5000);
+  function fillList(id, data) {
+    var target = $.getElementById(id);
+    var rets = [];
+    for (var i = 0; i < data.length; i++) {
+      var wk = data[i];
+      var it = '<li><a href="' + wk.url + '">' + wk.lang + '</a></li>';
+      rets.push(it);
+    }
+    target.innerHTML = rets.join('');
+  }
+
+  fillVersion();
+  fillList('wiki', wikis);
+  fillList('lang', docslangs);
 
 })(window);
